70a7a078d8f702b842dd2c76ecbfd09dad96e6b9
Amogh Mishra <amogh@kenome.io>
2018-04-28 00:09:59 +0530

    Add average friends by age

---
+++ average-friends-by-age.py
@@ -0,0 +1,28 @@
+from pyspark import SparkConf, SparkContext
+
+
+def find_friends_by_age():
+    conf = SparkConf().setMaster("local").setAppName("FriendsByAge")
+    sc = SparkContext(conf=conf)
+
+    social_media_dataset = sc.textFile(
+        "file:////Users/amoghmishra/Desktop/AmoghM/ApacheSpark/dataset/social_media_dataset.csv")
+    social_media_rdd = social_media_dataset.map(read_data)
+
+    key_value_rdd = social_media_rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+    avg_friends = key_value_rdd.mapValues(lambda x: x[0] / x[1])
+    age_avg_friends = avg_friends.collect()
+
+    for res_line in age_avg_friends:
+        print res_line
+
+
+def read_data(line):
+    line_split = line.split(",")
+    age = int(line_split[2])
+    friends = int(line_split[3])
+    return (age, friends)
+
+
+if __name__ == "__main__":
+    find_friends_by_age()
