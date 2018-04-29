5e471d97ae835abc28820c8189e80bfa0a84c30e
Amogh Mishra <amogh@kenome.io>
2018-04-29 19:37:46 +0530

    Add code for average #friends by age

--- average_friends_by_age.py
+++ average_friends_by_age.py
@@ -5,12 +5,13 @@ def find_friends_by_age():
     conf = SparkConf().setMaster("local").setAppName("FriendsByAge")
     sc = SparkContext(conf=conf)
 
+    #row_id, name, age, total friends
     social_media_dataset = sc.textFile(
         "file:////Users/amoghmishra/Desktop/AmoghM/ApacheSpark/dataset/social_media_dataset.csv")
-    social_media_rdd = social_media_dataset.map(read_data)
+    social_media_rdd = social_media_dataset.map(read_data) #parse each line of the dataset
 
-    key_value_rdd = social_media_rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
-    avg_friends = key_value_rdd.mapValues(lambda x: x[0] / x[1])
+    key_value_rdd = social_media_rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])) # count of people and sum of total friends
+    avg_friends = key_value_rdd.mapValues(lambda x: x[0] / x[1]) #average number of friends.
     age_avg_friends = avg_friends.collect()
 
     for res_line in age_avg_friends:
